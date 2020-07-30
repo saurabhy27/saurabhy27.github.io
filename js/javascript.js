4121451bf6c65c16387142f7d6c0f441d1c8a328
Saurabh Singh <isaurabh2709@gmail.com>
2020-07-30 17:52:14 +0530

    Color change and validated

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,12 @@
 function onClick(){
     window.open("files/SaurabhResume.pdf","_self");
 }
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector("body").style.visibility = "hidden"; 
+        document.querySelector("#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector("#loader").style.display = "none"; 
+        document.querySelector("body").style.visibility = "visible"; 
+        } 
+}; 
